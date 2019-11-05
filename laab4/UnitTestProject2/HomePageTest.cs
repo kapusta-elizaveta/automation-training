81865a3577635c17f234db112a22f3445d707280
Lizaveta Kapusta <kapustaeliz@gmail.com>
2019-11-05 09:10:02 +0300

    lab4

---
+++ laab4/UnitTestProject2/HomePageTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using laab4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace lab4Test
+{
+    [TestClass]
+    public class HomePageTest
+    {
+        public IWebDriver Driver { get; set; }
+        public WebDriverWait Wait { get; set; }
+
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            this.Driver = new ChromeDriver();
+           
+        }
+
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            this.Driver.Quit();
+        }
+
+        [TestMethod]
+        public void SignInTest()
+        {
+            HomePage homePage = new HomePage(this.Driver);
+            homePage.goToPage();
+            homePage.EntryGmail();
+            
+        }
+
+        [TestMethod]
+        public void SearchApartmentTest()
+        {
+            HomePage homePage = new HomePage(this.Driver);
+            homePage.goToPage();
+            homePage.ChooseArrivalDate();
+            homePage.ChooseDepartureDate();
+            homePage.ChooseVisitors();
+            homePage.ChooseApartment();
+            homePage.SearchApartment();
+        }
+
+
+    }
+}
---
+++ laab4/laab4/HomePage.cs
@@ -0,0 +1,104 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laab4
+{
+    public class HomePage
+    {
+        private IWebDriver driver;
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.ClassName, Using = "link-auth")]
+        public IWebElement ButtonPersonalAccount { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "js-login-canal-cab")]
+        public IWebElement TitlePersonalAccount { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "js-hbi-login-fb")]
+        public IWebElement FaceBookPersonalAccount { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "link-auth")]
+        public IWebElement ChooseHotels { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "react-datepicker-wrapper")]
+        public IWebElement ArrivalDate { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@aria-label='day-20']")]
+        public IWebElement TittleArrivalDate { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "react-datepicker-wrapper")]
+        public IWebElement DepartureDate { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@aria-label='day-23']")]
+        public IWebElement TittleDepartureDate { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "c0115")]
+        public IWebElement Visitors { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "c0138")]
+        public IWebElement VisitorsPlus { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "c0119")]
+        public IWebElement Apartment { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "c0138")]
+        public IWebElement ApartmentPlus { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "c016")]
+        public IWebElement SearchButton { get; set; }
+
+
+        public void goToPage()
+        {
+            driver.Navigate().GoToUrl("https://azimuthotels.com/");
+        }
+
+        public void EntryGmail()
+        {
+            this.ButtonPersonalAccount.Click();
+            this.TitlePersonalAccount.Click();
+            this.FaceBookPersonalAccount.Click();
+        }
+
+        public void ChooseArrivalDate()
+        {
+            this.ArrivalDate.Click();
+            this.TittleArrivalDate.Click();
+         }
+
+        public void ChooseDepartureDate()
+        {
+            this.DepartureDate.Click();
+            this.TittleDepartureDate.Click();
+
+        }
+
+        public void ChooseVisitors()
+        {
+            this.Visitors.Click();
+            this.VisitorsPlus.Click();
+         
+        }
+
+        public void ChooseApartment()
+        {
+            this.Apartment.Click();
+            this.Apartment.Click();
+        }
+
+        public void SearchApartment()
+        {
+            this.SearchButton.Click();
+        }
+    }
+}
