e451783d371b46af0ee74067937e69bdca54711f
Kapusta Liza <kapustaeliz@gmail.com>
2019-12-22 23:13:45 +0300

    update

---
+++ frameworkTwo/Framework/Framework/Model/SearchQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Framework.Model
+{
+    public class SearchQuery
+    {
+        public string City { get; set; }
+        public string ArrivalDate { get; set; }
+        public string DepartureDate { get; set; }
+        public string Visitors { get; set; }
+
+        
+    }
+}
